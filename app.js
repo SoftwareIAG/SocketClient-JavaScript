db62ff90ebe1991b021bb1a3d0e1252f757244df
Ian Torres <iantorres@outlook.com>
2021-03-02 04:12:34 -0300

    Client updated.

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const client = new net.Socket();
 
 client.connect(25503, "127.0.0.1", () => {
     console.log("Connected");
-    client.write("Hello, server!");
+    client.write("First Ping ... \n");
 });
 
 client.on("data", (data) => {
    console.log("Received: " + data);
+
+   setTimeout(() => {
+       let sent = "Pong ... \n";
+       client.write(sent);
+       console.log("Send: " + sent);
+   }, 3000);
 });
 
 client.on("close", () => {
